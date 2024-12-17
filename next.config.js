ed5325841add930e852e9517a61c614e008e7bd6
Syed-MuhammadRazaZaidi <raza.zaidi@ymail.com>
2024-12-17 19:55:43 +0500

    Adding CLERK_PUBLISHABLE_KEY

--- next.config.js
+++ next.config.js
@@ -1,15 +1,17 @@
-
-  module.exports = {
-    images: {
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: 'images.unsplash.com',
-        },
-        {
-          protocol: 'https',
-          hostname: 'plus.unsplash.com',
-        },
-      ],
-    },
-  }
+module.exports = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "(link unavailable)",
+      },
+      {
+        protocol: "https",
+        hostname: "(link unavailable)",
+      },
+    ],
+  },
+  env: {
+    CLERK_PUBLISHABLE_KEY: "pk_test_b3B0aW1hbC1ob3JuZXQtNTkuY2xlcmsuYWNjb3VudHMuZGV2JA",
+  },
+};

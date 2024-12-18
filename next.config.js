b89573fc1ace2cc92ccc7c1af3f1101e000f364c
Syed-MuhammadRazaZaidi <raza.zaidi@ymail.com>
2024-12-18 16:10:45 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -12,6 +12,9 @@ module.exports = {
     ],
   },
   env: {
-    CLERK_PUBLISHABLE_KEY: "pk_test_b3B0aW1hbC1ob3JuZXQtNTkuY2xlcmsuYWNjb3VudHMuZGV2JA",
+    CLERK_PUBLISHABLE_KEY:
+      "pk_test_b3B0aW1hbC1ob3JuZXQtNTkuY2xlcmsuYWNjb3VudHMuZGV2JA",
+    CLERK_SECRET_KEY: "sk_test_xW4y1gUKymQ1qbhB9GtqnvYTbtVbG15nYxfuYoTFSF",
+    CLERK_ENCRYPTION_KEY: "sk_test_xW4y1gUKymQ1qbhB9GtqnvYTbtVbG15nYxfuYoTFSF",
   },
 };
